44bf85fe37d8ab494237dca88892762c46f49200
jjjkkkjjj <kado_89_ssp_jun@yahoo.co.jp>
2021-05-15 19:00:24 +0900

    stride sample

--- test.cpp
+++ test.cpp
@@ -1,7 +1,35 @@
 #include <stdio.h>
 #include "umesimd/UMESimd.h"
 
+#define TESTSIZE 100
+#define SIMDSIZE 32
+
+
+void testsimd(){
+    int iteration, remainder, offset;
+    float a[TESTSIZE] = { 0.0f };
+    uint32_t stride = 2;
+    UME::SIMD::SIMDVec_f<float, SIMDSIZE> vec;
+
+    iteration = TESTSIZE / SIMDSIZE;
+    remainder = TESTSIZE % SIMDSIZE;
+
+    for (int i = 0; i < iteration; i++){
+        offset = i*stride*SIMDSIZE;
+        vec.gatheru(&a[offset], stride);
+        vec += 3.14;
+        vec.scatteru(&a[offset], stride);
+    }
+    for (int i = 0; i < remainder; i++){
+        a[offset + i] += 3.14;
+    }
+    
+    for (int i = 0; i < TESTSIZE; i++){
+        printf("%f\n", a[i]);
+    }
+}
+
 int main(){
-    printf("Hello World");
+    testsimd();
     return 0;
 }
